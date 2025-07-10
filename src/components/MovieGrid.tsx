3b04c480123fa91cc9348c8eac9a6993ea89cf46
prathvi-kanani113 <parthvi.kanani@bankaiinformatics.co.in>
2025-07-10 11:48:44 +0530

    added comment

--- src/components/MovieGrid.tsx
+++ src/components/MovieGrid.tsx
@@ -12,6 +12,7 @@ const MovieGrid: React.FC<Props> = ({ movies }) => (
   <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
     {movies.map((movie) => (
       <Grid key={movie.id} size={{ xs: 2, sm: 4, md: 2 }}>
+        {/* card render */}
         <MovieCard movie={movie} />
       </Grid>
     ))}
